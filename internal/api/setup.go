3c0c9e4d4f84be1b358f7b60919c84ff26e6e219
Matteo Stefanini <dev@mstefanini.com>
2024-01-27 01:39:38 +0100

    fix: wrong listen address

--- internal/api/setup.go
+++ internal/api/setup.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-var addr = flag.String("addr", "localhost:8080", "api service address")
+var addr = flag.String("addr", "0.0.0.0:8080", "api service address")
 
 func Serve() {
 	http.HandleFunc("/", HomeHandler)
