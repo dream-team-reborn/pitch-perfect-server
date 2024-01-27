d251907385528faa8a8c47ee28dbc601036d7c63
Matteo Stefanini <dev@mstefanini.com>
2024-01-27 01:27:22 +0100

    feat(home): add home http api for check

--- internal/api/setup.go
+++ internal/api/setup.go
@@ -9,6 +9,7 @@ import (
 var addr = flag.String("addr", "localhost:8080", "api service address")
 
 func Serve() {
+	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/ws", WsHandler)
 	http.HandleFunc("/login", LoginHandler)
 
