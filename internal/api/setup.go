2f9115a785e22dc61bfc93a3c6bd6bde5ff9104c
Matteo Stefanini <dev@mstefanini.com>
2024-01-28 20:52:07 +0100

    feat(https): add https certificate

--- internal/api/setup.go
+++ internal/api/setup.go
@@ -1,12 +1,13 @@
 package api
 
 import (
-	"flag"
+	"crypto/tls"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/acme/autocert"
 	"net/http"
 )
 
-var addr = flag.String("addr", "0.0.0.0:8080", "api service address")
+//var addr = flag.String("addr", "0.0.0.0:8080", "api service address")
 
 func Serve() {
 	r := mux.NewRouter()
@@ -14,6 +15,22 @@ func Serve() {
 	r.HandleFunc("/ws", WsHandler)
 	r.HandleFunc("/login", LoginHandler)
 	r.HandleFunc("/config", ConfigHandler)
-	http.Handle("/", r)
-	_ = http.ListenAndServe(*addr, nil)
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("pitch-perfect.mstefanini.com"),
+		Cache:      autocert.DirCache("./certs"), //Folder for storing certificates
+	}
+
+	server := &http.Server{
+		Addr:    ":https",
+		Handler: r,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+
+	server.ListenAndServeTLS("", "") //Key and cert are coming from Let's Encrypt
 }
